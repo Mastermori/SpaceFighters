c4c925e7cc37aa9fef8a6272314d36ee3710a821
Mastermori <mastermoriii@gmail.com>
2020-02-08 00:45:42 +0100

    changed cloud timing and adjusted for sprite usage in level1

--- levels/Level1.gd
+++ levels/Level1.gd
@@ -1,26 +1,29 @@
 extends Node2D
 
-export var cloud_timer_min := 1.0
-export var cloud_timer_max := 3.0
+export var cloud_timer_min := 3.0
+export var cloud_timer_max := 12.0
 export var cloud_scale_rand := 0.5
 
 export(Array, Texture) var cloud_types : Array
-export var cloud_speed := 200
+export var cloud_speed := 100
 
 onready var cloud_timer := $CloudTimer
 
 func _ready():
-	print("Ready Level")
+	spawn_random_cloud()
 	start_timer()
 
 func start_timer():
 	cloud_timer.start(cloud_timer_min + randf() * cloud_timer_max)
 
-func _on_CloudTimer_timeout():
-	var cloud : TextureRect = preload("res://clouds/Cloud.tscn").instance()
-	cloud.init(Vector2(cloud_speed, 0))
+func spawn_random_cloud():
+	var cloud : Sprite = preload("res://clouds/Cloud.tscn").instance()
+	cloud.init(Vector2(0, cloud_speed * randf() * .2 + cloud_speed ))
 	cloud.texture = cloud_types[randi() % cloud_types.size()]
-	cloud.rect_position.y = -cloud.rect_size.y
+	cloud.position.y = -cloud.texture.get_size().y * cloud.scale.y
 	$Clouds.add_child(cloud)
+
+func _on_CloudTimer_timeout():
+	spawn_random_cloud()
 	start_timer()
 	print("Cloud")
