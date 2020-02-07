8056ada7c48ba540518b59cfc16ddda693b95b1e
Mastermori <mastermoriii@gmail.com>
2020-02-07 11:59:58 +0100

    added keep in bounds functionality to player

--- player/Player.gd
+++ player/Player.gd
@@ -8,16 +8,20 @@ export var acceleration := 1500
 export var deceleration := 3000
 export var max_speed := 500
 
-var invincible := false
+export(RectangleShape2D) var keep_in_rect : RectangleShape2D
+
 
 var vel := Vector2.ZERO
 
+var invincible := false
 var lock := false
+
 
 onready var anim_player = $AnimationPlayer
 onready var resawn_timer = $RespawnTimer
 onready var invin_timer = $InvincibilityTimer
 
+onready var spawn_position := position
 
 func _ready():
 	respawn()
@@ -35,6 +39,7 @@ func _physics_process(delta):
 			anim_player.play("fly_left")
 		else:
 			anim_player.play("fly_straight")
+	keep_in_bounds(delta)
 	vel = move_and_slide(vel)
 
 func get_move_dir() -> Vector2:
@@ -42,6 +47,13 @@ func get_move_dir() -> Vector2:
 	dir.x += Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
 	dir.y += Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
 	return dir
+
+func keep_in_bounds(delta):
+	var spawn_dist = position - spawn_position
+	if abs(spawn_dist.x) > keep_in_rect.extents.x:
+		position.x -= spawn_dist.normalized().x * pow(abs(spawn_dist.x)-keep_in_rect.extents.x, 1.5) * delta
+	if abs(spawn_dist.y) > keep_in_rect.extents.y:
+		position.y -= spawn_dist.normalized().y * pow(abs(spawn_dist.y)-keep_in_rect.extents.y, 1.5) * delta
 
 func die():
 	anim_player.play("die")
