d9afb9660c9db8cd06b3775056a81c655e192f9f
Mastermori <mastermoriii@gmail.com>
2020-02-08 00:44:07 +0100

    changed clouds to Sprites in code
    forgotten in original commit

--- clouds/Cloud.gd
+++ clouds/Cloud.gd
@@ -1,4 +1,4 @@
-extends TextureRect
+extends Sprite
 
 var vel := Vector2.ZERO
 
@@ -6,7 +6,7 @@ func init(speed : Vector2):
 	vel = speed
 
 func _process(delta):
-	rect_position += vel * delta
+	position += vel * delta
 
 func _on_VisibilityNotifier2D_screen_exited():
 	print("Cloud left!")
