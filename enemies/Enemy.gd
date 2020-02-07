f7fe9f78788f86b1e9fa6ae363144e067e501def
Mastermori <mastermoriii@gmail.com>
2020-02-08 00:44:49 +0100

    added Enemy with strafer behaviour
    follows a parent PathFollow2D node

--- enemies/Enemy.gd
+++ enemies/Enemy.gd
@@ -1,3 +1,23 @@
 extends Character
 
 class_name Enemy
+
+export var onscreen_speed := 350.0
+export var offscreen_speed := 50
+
+var speed := offscreen_speed
+
+onready var follow : PathFollow2D = get_parent()
+
+func _process(delta):
+	follow.offset += speed * delta
+
+
+func die():
+	.die()
+
+func _on_VisibilityNotifier2D_screen_entered():
+	speed = onscreen_speed
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
