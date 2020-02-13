d16b575ba6b819cd0ac7193d19ba68a9ea74ad08
Mastermori <mastermoriii@gmail.com>
2020-02-14 00:13:46 +0100

    removed debug prints

--- enemies/EnemyGroup.gd
+++ enemies/EnemyGroup.gd
@@ -13,12 +13,10 @@ export var offset_mask := Vector2.ZERO
 func _ready():
 	if global_position.y >= 0:
 		print("ERROR: Enemy groups must be located above the level")
-	print("Group position: " + str(global_position))
 	for i in range(amount):
 		var enemy : Enemy = construct()
 		enemy.position = offset * i + offset_step * offset_mask * i
 		add_child(enemy)
-		print("Enemy" + str(i) + " position: " + str(enemy.global_position))
 
 func construct() -> Enemy:
 	return enemy_scene.instance()
