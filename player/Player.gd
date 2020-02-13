50937ddf2ce38b70dd422fc8987c0fcc4ada0fc2
Mastermori <mastermoriii@gmail.com>
2020-02-13 22:07:33 +0100

    Removed old player controlls
    will add new ones next

--- player/Player.gd
+++ player/Player.gd
@@ -25,52 +25,11 @@ func _ready():
 
 func _physics_process(delta):
 	if not lock:
-		var move_dir := get_move_dir()
-		if use_mouse_movement:
-			if move_dir.length() > stop_distance:
-				vel = move_dir.normalized() * max_speed
-			else:
-				global_position = get_global_mouse_position()
-				player_anims.play("fly_straight")
-		else:
-			if move_dir == Vector2.ZERO:
-				apply_friction(deceleration * delta)
-				player_anims.play("fly_straight")
-			else:
-				apply_movement(move_dir * acceleration * delta)
-		if move_dir.x > 0:
-			if vel.x > max_speed * .9:
-				player_anims.play("fly_right")
-			else:
-				player_anims.play("fly_right_slight")
-		elif move_dir.x < 0:
-			if vel.x < -max_speed * .9:
-				player_anims.play("fly_left")
-			else:
-				player_anims.play("fly_left_slight")
-		else:
-			player_anims.play("fly_straight")
 		
-		keep_in_bounds(delta)
-		
-		var collided_with = move_and_collide(vel * delta)
-		
-		if collided_with:
-			if collided_with is Strafer:
-				pass
 		
 		if Input.is_action_pressed("player_shoot") and shoot_timer.is_stopped():
 			shoot_projectile(preload("res://projectiles/OrangeProjectile.tscn").instance(), Vector2.UP * shot_speed)
 			shoot_timer.start(shoot_delay)
-
-func get_move_dir() -> Vector2:
-	var dir := Vector2.ZERO
-	if use_mouse_movement:
-		dir = (get_global_mouse_position() - global_position)
-	else:
-		dir.x += Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
-		dir.y += Input.get_action_strength("player_down") - Input.get_action_strength("player_up")
-	return dir
 
 func keep_in_bounds(delta):
 	var spawn_dist = position - spawn_position
