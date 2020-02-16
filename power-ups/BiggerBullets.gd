395c331965e21dbc44bf6ec82910a55f1ededc23
Mastermori <mastermoriii@gmail.com>
2020-02-16 03:04:25 +0100

    added bigger bullets power-up

---
+++ power-ups/BiggerBullets.gd
@@ -0,0 +1,5 @@
+extends PowerUp
+
+func give_powerup(player : Player):
+	print("powerup activated")
+	player.add_power_up("bigger_bullets", ["bullet_size", "bullet_damage_scale"], [Vector2(1, 1), .5], 5)
