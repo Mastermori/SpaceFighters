678e9aa9ef4dd090fcc4de2f4e60ed0147704cfb
Mastermori <mastermoriii@gmail.com>
2020-02-17 20:35:45 +0100

    Added Test button to Title Screen (for debug purposes)

--- src/menus/scripts/TitleScreen.gd
+++ src/menus/scripts/TitleScreen.gd
@@ -2,10 +2,13 @@ extends Control
 
 
 
-
 func _on_StartButton_pressed():
 	Globals.find_level("Level1")
 
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
+
+
+func _on_TestButton_pressed():
+	pass
